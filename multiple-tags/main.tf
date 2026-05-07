28ef24a06b1367a9c7da0f7f3e10d1612018e1c8
akshayenuguru <akshay@rafay.co>
2026-05-07 13:14:15 +0530

    Add main.tf for AWS instance configuration

---
+++ multiple-tags/main.tf
@@ -0,0 +1,67 @@
+# main.tf
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      env   = "qa"
+      email = "akshay@rafay.co"
+    }
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami               = var.ami_id
+  instance_type     = var.instance_type
+  availability_zone = var.aws_region_az
+
+  tags = {
+    Name        = var.name
+    env         = "qa"
+    email       = var.email
+    project     = var.tag_project
+    team        = var.tag_team
+    owner       = var.tag_owner
+    cost_center = var.tag_cost_center
+    application = var.tag_application
+  }
+
+  volume_tags = {
+    env   = "qa"
+    email = "akshay@rafay.co"
+  }
+}
+
+resource "aws_instance" "ubuntu-1" {
+  ami               = var.ami_id
+  instance_type     = var.instance_type
+  availability_zone = var.aws_region_az
+
+  tags = {
+    Name        = var.name1
+    env         = "qa"
+    email       = var.email
+    project     = var.tag_project
+    team        = var.tag_team
+    owner       = var.tag_owner
+    cost_center = var.tag_cost_center
+    application = var.tag_application
+  }
+
+  volume_tags = {
+    env   = "qa"
+    email = "akshay@rafay.co"
+  }
+
+  depends_on = [aws_instance.ubuntu]
+}
